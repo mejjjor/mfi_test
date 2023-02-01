36c734c065d7858d825c213c90c5feeef9dd8f37
Erik Aouizerate <erik.aouizerate@mintset.io>
2023-02-01 12:08:17 +0100

    feat: Add forecast for map pins

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -1,30 +1,63 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 
+import Feature from "ol/Feature.js";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import OSM from "ol/source/OSM.js";
+import { fromLonLat } from "ol/proj.js";
+import Point from "ol/geom/Point.js";
+import { Icon, Style } from "ol/style.js";
+import { toLonLat } from "ol/proj";
+import Select from "ol/interaction/Select.js";
 
 import {
   DragRotateAndZoom,
   defaults as defaultInteractions,
 } from "ol/interaction.js";
 
-export default function MapWrapper() {
+function createStyle(src) {
+  return new Style({
+    image: new Icon({
+      crossOrigin: "anonymous",
+      src,
+    }),
+  });
+}
+
+export default function MapWrapper({ fetchForcast }) {
   const mapElement = useRef();
 
-  const [map, setMap] = useState(null);
-  const [featuresLayer, setFeaturesLayer] = useState(null);
-  const [selectedCoord, setSelectedCoord] = useState(null);
-  mapElement.current = map;
+  const mfiToulouse = new Feature({
+    geometry: new Point(fromLonLat([1.3772919, 43.5787476])),
+  });
+
+  const mfSaintMande = new Feature({
+    geometry: new Point(fromLonLat([2.4223775, 48.8456978])),
+  });
+
+  const wmoGeneve = new Feature({
+    geometry: new Point(fromLonLat([6.1440339, 46.2233499])),
+  });
+
+  mfiToulouse.setStyle(createStyle("assets/map-pin-filled.svg"));
+  mfSaintMande.setStyle(createStyle("assets/map-pin-filled.svg"));
+  wmoGeneve.setStyle(createStyle("assets/map-pin-filled.svg"));
+
+  mfiToulouse.set("label", "MFI Toulouse (France)");
+  mfSaintMande.set("label", "MF Saint Mandé (France)");
+  wmoGeneve.set("label", "WMO Genève (Suisse)");
 
   useEffect(() => {
+    const initialVertorLayer = new VectorSource({
+      features: [mfiToulouse, mfSaintMande, wmoGeneve],
+    });
     const initalFeaturesLayer = new VectorLayer({
-      source: new VectorSource(),
+      source: initialVertorLayer,
     });
 
     const initialMap = new Map({
@@ -45,12 +78,40 @@ export default function MapWrapper() {
       controls: [],
     });
 
-    setMap(initialMap);
-    setFeaturesLayer(initalFeaturesLayer);
+    initialMap.on("click", function (evt) {
+      const feature = initialMap.forEachFeatureAtPixel(
+        evt.pixel,
+        function (feature) {
+          return feature;
+        }
+      );
+      if (!feature) {
+        return;
+      }
+      var lonlat = toLonLat((feature.getGeometry() as Point).getCoordinates());
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+
+      fetchForcast({ lon, lat, label: feature.get("label") });
+    });
+
+    const select = new Select({
+      style: function (feature: any) {
+        feature.setStyle(createStyle("assets/map-pin-filled-selected.svg"));
+      },
+    });
+
+    initialMap.addInteraction(select);
+
+    initialMap.on("pointermove", function (evt) {
+      const pixel = initialMap.getEventPixel(evt.originalEvent);
+      const hit = initialMap.hasFeatureAtPixel(pixel);
+
+      (initialMap.getTarget() as HTMLElement).style.cursor = hit
+        ? "pointer"
+        : "";
+    });
   }, []);
 
-  // get ref to div element - OpenLayers will render into this div
-
-  // initialize map on first render - logic formerly put into componentDidMount
   return <div ref={mapElement} className="w-screen h-screen"></div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,56 @@
 "use client";
 
-import { Autocomplete, Select } from "@mantine/core";
+import { Autocomplete } from "@mantine/core";
 import { useEffect, useState } from "react";
 import Map from "./Map";
 import ky from "ky";
 import { Loader } from "@mantine/core";
-
+import { Accordion } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
 import Image from "next/image";
+import dayjs from "dayjs";
 
 export default function Page() {
   const [suggestions, setSuggestions] = useState([]);
   const [autocompleteValue, setAutocompleteValue] = useState("");
   const [debouncedValue] = useDebouncedValue(autocompleteValue, 300);
   const [loading, setLoading] = useState(false);
+  const [loadingForcast, setLoadingForecast] = useState(false);
   const [isValueSelected, setIsValueSelected] = useState(false);
 
+  const [forecast, setForecast] = useState([]);
+
   const handleSubmit = (val) => {
-    console.log("dddddd", val);
     setIsValueSelected(true);
+    fetchForecast({ lat: val.lat, lon: val.lon });
   };
 
   const fetchSuggestion = async () => {
     return (await ky(
-      `${process.env.NEXT_PUBLIC_NOMINATIM_OSM_URL}/search?city=${debouncedValue}&format=json&addressdetails=1&limit=16`
+      `${process.env.NEXT_PUBLIC_NOMINATIM_OSM_URL}/search?q=${debouncedValue}&format=json&addressdetails=1&limit=16`
     ).json()) as Array<any>;
+  };
+
+  const fetchForecast = async ({ lon, lat, label = "" }) => {
+    setLoadingForecast(true);
+    if (label) {
+      setIsValueSelected(true);
+      setAutocompleteValue(label);
+    }
+    const results = (await ky(
+      `${process.env.NEXT_PUBLIC_OPENWEATHER_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_APIKEY}&units=metric`
+    ).json()) as any;
+
+    setForecast(
+      results.daily.slice(0, 3).map((forecast) => ({
+        temp: forecast.temp.day.toFixed(1),
+        weather: {
+          label: forecast.weather[0].main,
+          icon: forecast.weather[0].icon,
+        },
+      }))
+    );
+    setLoadingForecast(false);
   };
 
   useEffect(() => {
@@ -45,7 +71,6 @@ export default function Page() {
               item.address.town ||
               item.address.country
             } (${item.address.country})`,
-            // value: `${item.osm_id}/${item.lat}/${item.lon}`,
             lat: item.lat,
             lon: item.lon,
           };
@@ -57,8 +82,6 @@ export default function Page() {
           return acc;
         }, {});
 
-      console.log("sugg", newSuggestions);
-
       setSuggestions(Object.values(newSuggestions));
       setLoading(false);
     });
@@ -66,33 +89,57 @@ export default function Page() {
 
   return (
     <div className="relative">
-      <div className="fixed top-2 left-2 z-10 bg-white rounded p-2 w-[250px]">
-        <Autocomplete
-          label="Ville"
-          rightSection={loading ? <Loader size={16} /> : null}
-          placeholder="Paris"
-          onChange={setAutocompleteValue}
-          value={autocompleteValue}
-          onItemSubmit={handleSubmit}
-          data={suggestions}
-        />
-        <h3>Prévision méteo</h3>
-        <div>
-          {[12, 13, 15].map((forecast, index) => (
-            <div key={index} className="flex items-center text-lg gap-2">
-              <div>j+{index + 1} :</div>
-              <div>{forecast} °C</div>
-              <Image
-                alt="forecast icon"
-                src={"http://openweathermap.org/img/wn/10d@4x.png"}
-                width="50"
-                height="50"
+      <div className="fixed top-2 left-2 z-10 bg-white rounded p-2 min-w-[250px]">
+        <Accordion defaultValue="content">
+          <Accordion.Item className="border-none" value="content">
+            <Accordion.Control>
+              <h4 className="m-1">MFI TEST / Erik A.</h4>
+              <Autocomplete
+                onClick={(e) => e.stopPropagation()}
+                label="Ville"
+                rightSection={loading ? <Loader size={16} /> : null}
+                placeholder="Paris"
+                onChange={setAutocompleteValue}
+                value={autocompleteValue}
+                onItemSubmit={handleSubmit}
+                data={suggestions}
               />
-            </div>
-          ))}
-        </div>
+            </Accordion.Control>
+            <Accordion.Panel>
+              <h3 className="mb-0">Prévisions méteo</h3>
+              <div className="flex justify-center">
+                {loadingForcast && <Loader />}
+              </div>
+              {!loadingForcast && (
+                <div className="inline-block justify-center bg-stone-500">
+                  {forecast.map((forecast, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center justify-between text-lg bg-white mb-[1px] last:mb-0 px-2 gap-[1px]"
+                    >
+                      <div>
+                        j+{index + 1} (
+                        {dayjs()
+                          .add(index + 1, "day")
+                          .format("DD/MM")}
+                        ) :{" "}
+                        <span className="font-bold">{forecast.temp} °C</span>
+                      </div>
+                      <Image
+                        alt={forecast.weather.label}
+                        src={`http://openweathermap.org/img/wn/${forecast.weather.icon}@2x.png`}
+                        width="50"
+                        height="50"
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
       </div>
-      <Map />;
+      <Map fetchForcast={fetchForecast} />;
     </div>
   );
 }
