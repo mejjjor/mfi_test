4bf9c50c16fa8e6896d9bddcbc2d55e2d3097782
Erik Aouizerate <erik.aouizerate@mintset.io>
2023-02-01 18:53:34 +0100

    fix: Add seo description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export default async function Layout({ children }) {
     <html lang="fr">
       <head>
         <title>MFI TEST / ERIK A</title>
+        <meta name="description" content="MFI - test technique Pour Erik A." />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
